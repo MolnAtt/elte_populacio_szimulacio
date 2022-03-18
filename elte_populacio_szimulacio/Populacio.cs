6d496b4783ff5a91fb6231ade394cd677cf6bd5c
Molnár Attila <molnar.attila@szlgbp.hu>
2022-03-18 16:28:06 +0100

    Add project files.

---
+++ elte_populacio_szimulacio/Populacio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace elte_populacio_szimulacio
+{
+    class Populacio
+    {
+        double[] H, Sz;
+        Random r;
+        List<int> T;
+
+
+        public Populacio(double[] H, double[] Sz, int N)
+        {
+            this.H = H;
+            this.Sz = Sz;
+            this.r = new Random();
+            this.T = new List<int>(20 * N);
+            for (int i = 0; i < N; i++)
+            {
+                T.Add(1);
+            }
+        }
+
+        public void Szimulációs_lépés()
+        {
+            for (int i = 0; i < T.Count; i++)
+            {
+                if (Véletlen<H[T[i]])
+                    T[i] = 0;
+                else
+                    if (Véletlen<Sz[T[i]])
+                        T.Add(1);
+            }
+            Tömörítés();
+        }
+
+        double Véletlen { get => r.NextDouble(); } 
+
+        public void Tömörítés()
+        {
+            for (int i = 0; i < T.Count; i++)
+                if (T[i] == 0)
+                    T.RemoveAt(i--);
+        }
+
+        public void Diagnosztika()
+        {
+            foreach (int item in T)
+                Console.Write($"{item} ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ elte_populacio_szimulacio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elte_populacio_szimulacio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] H = new double[] { 0.1, 0.2, 0.2, 0.2, 1 };
+            double[] Sz = new double[] { 0.1, 0.2, 0.2, 0.2, 0 };
+            Populacio P = new Populacio(H, Sz, 10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                P.Szimulációs_lépés();
+                P.Diagnosztika();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
